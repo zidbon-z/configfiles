0e8a344dfd370a7a8a93b45b99aca2f25bda2e43
zidbon <zidbon@hotmail.com>
2022-11-12 01:27:43 -0800

    Added another theme to theme_scripts

--- theme_scripts/theme1/theme1.sh
+++ theme_scripts/theme1/theme1.sh
@@ -2,19 +2,15 @@
 
 # change bspwm
 cat $HOME/.config/configfiles/theme_scripts/theme1/bspwm_theme1 > $HOME/.config/bspwm/themes/theme
+
 # change nitrogen
-nitrogen --set-scaled ~/.config/configfiles/theme_scripts/theme1/img.jpg
+nitrogen --set-scaled ~/.config/configfiles/theme_scripts/theme1/img.jpg --save
+
 # change kitty
 cat $HOME/.config/configfiles/theme_scripts/theme1/kitty_theme1 > $HOME/.config/kitty/kitty.conf
 
 # change bash_prompt
-if [ -f "$HOME/.bash_prompt" ]                                                          
-then                                                                                
-  rm "$HOME/.bash_prompt"                                                               
   cat  $HOME/.config/configfiles/theme_scripts/theme1/bash_prompt_theme1 > $HOME/.bash_prompt                      
-else                                                                                
-  cat  $HOME/.config/configfiles/theme_scripts/theme1/bash_prompt_theme1 > $HOME/.bash_prompt                      
-fi    
 
 # change rofi
 cat ~/.config/configfiles/theme_scripts/theme1/rofi_theme1 > ~/.config/rofi/config.rasi
---
+++ theme_scripts/theme2/theme2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# change bspwm
+cat $HOME/.config/configfiles/theme_scripts/theme2/bspwm_theme2 > $HOME/.config/bspwm/themes/theme
+# change nitrogen
+nitrogen --set-scaled ~/.config/configfiles/theme_scripts/theme2/img.jpg --save
+# change kitty
+cat $HOME/.config/configfiles/theme_scripts/theme2/kitty_theme2 > $HOME/.config/kitty/kitty.conf
+
+# change bash_prompt
+  cat  $HOME/.config/configfiles/theme_scripts/theme2/bash_prompt_theme2 > $HOME/.bash_prompt                      
+
+# change rofi
+cat ~/.config/configfiles/theme_scripts/theme2/rofi_theme2 > ~/.config/rofi/config.rasi
+
+# reload bspwm
+~/.config/bspwm/bspwmrc
