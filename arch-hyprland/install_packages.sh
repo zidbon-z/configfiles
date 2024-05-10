0c3243aae3fc127f0fdd20105bd21eee71ea1222
zidbon <zidbon@hotmail.com>
2024-05-10 11:12:37 -0700

    Working on the install_packages.sh script for Hyprland

--- arch-hyprland/install_packages.sh
+++ arch-hyprland/install_packages.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
-sudo pacman -S --noconfirm  kitty alsa-utils udiskie thunar git dmenu ntfs-3g lxappearance qt5ct breeze-gtk deepin-gtk-theme ncdu firefox os-prober openssh  virtualbox newsboat vlc mpv calcurse w3m python-pillow irssi figlet btop neofetch flameshot toilet ncmpcpp mpd mpc hyprland waybar waypaper wofi
+sudo pacman -Syy
+sudo pacman -Syu
+mkdir /tmp/yay
+cd /tmp/yay
+curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
+makepkg -si
+cd
+rm -rf /tmp/yay
+
+yay -S --noconfirm  kitty alsa-utils udiskie thunar git dmenu ntfs-3g lxappearance qt5ct breeze-gtk deepin-gtk-theme ncdu firefox os-prober openssh  virtualbox newsboat vlc mpv calcurse w3m python-pillow irssi figlet btop neofetch flameshot toilet ncmpcpp mpd mpc hyprland waybar waypaper wofi
